7997e3a63cbf25717a70d90fd555216dfc538c1e
Shubhamskg <shubhamkumargupta786@gmail.com>
2025-01-07 03:14:36 +0530

    sfesadf

--- app/api/events/[eventId]/register/route.js
+++ app/api/events/[eventId]/register/route.js
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import Event from '@/models/Event';
 import Registration from '@/models/Registration';
-import { sendEmail } from '@/lib/email';
+import { sendEmail } from '@/lib/mail';
 
 export async function POST(request, { params }) {
   try {
